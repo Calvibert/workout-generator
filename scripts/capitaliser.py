d4bbd799af8db7174a3313e6803210a9103c964f
Samuel Dufresne <samuel.dufresne@live.com>
2018-09-05 19:57:45 -0400

    Complete capitaliser

--- scripts/capitaliser.py
+++ scripts/capitaliser.py
@@ -1,2 +1,40 @@
-# Capitalize the exercises
+# Capitalize a file.
+# Used for our exercises.
 
+import sys
+
+
+print('Enter the file name: ')
+filename = sys.stdin.readline()
+filename = filename[0:len(filename)-1]
+
+f = open(filename, 'r')
+
+exl = []
+
+for ex in f:
+    count = 0
+    space = True
+    l = list(ex)
+    for i in l:
+        if space:
+            l[count] = i.upper()
+            space = False
+        if i == ' ':
+            space = True
+        count = count+1
+        
+    exl.append("".join(l).rstrip())
+
+
+print(exl)
+output_filename = 'sorted-' + filename
+o_stdout = sys.stdout
+f = open(output_filename, 'w')
+sys.stdout = f
+
+for i in exl:
+    print(i)
+
+sys.stdout = o_stdout
+f.close()
